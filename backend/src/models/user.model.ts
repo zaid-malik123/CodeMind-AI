8a5fce3ae43d741cbc74c689ea696c89534a88a5
Zaid Malik <mzaidmalik25@gmail.com>
2026-06-13 09:20:44 +0530

    feat: update user model to include provider field and conditional password requirement

--- backend/src/models/user.model.ts
+++ backend/src/models/user.model.ts
@@ -1,28 +1,36 @@
 import mongoose, { Document } from "mongoose";
 
-
 export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
   imageUrl?: string;
+  provider: "local" | "google";
   isVerified: boolean;
 }
-
 
 const userSchema = new mongoose.Schema<IUser>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: {
+      type: String,
+      required: function(this: IUser) {
+        return this.provider === "local";
+      },
+    },
     imageUrl: { type: String },
-    isVerified: { type: Boolean, default: false}
+    provider: {
+      type: String,
+      enum: ["local", "google"],
+      default: "local",
+    },
+    isVerified: { type: Boolean, default: false },
   },
   {
     timestamps: true,
   },
 );
-
 
 const User = mongoose.model<IUser>("User", userSchema);
 
