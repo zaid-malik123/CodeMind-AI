654129291a4b4a54831ea41a6d4171892ec30eea
Zaid Malik <mzaidmalik25@gmail.com>
2026-07-25 14:25:40 +0530

    refactor: remove unused icon imports from Repo component

--- frontend/src/app/(protected)/repo/page.tsx
+++ frontend/src/app/(protected)/repo/page.tsx
@@ -6,12 +6,11 @@ import RepoSkeletonCard from "@/components/RepoSkeletonCard";
 
 import {
   Search,
-  GitFork,
-  Star,
-  Eye,
+  
+ 
   Plus,
   MoreVertical,
-  GitBranch,
+ 
   Clock,
   Globe,
   FileCode,
